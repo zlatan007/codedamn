d998bff4703eb1aa997a4b31ad69230ec033fe9f
zlatan007 <shuklaharshit00007@gmail.com>
2022-03-05 20:28:11 +0530

    pratice hooks

---
+++ src/Components/Pratice.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+const Pratice = () => {
+  return (
+    <div>Pratice</div>
+  )
+}
+
+export default Pratice;
--- src/Sample.js
+++ src/Sample.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react'
+
+// const Sample = () => {
+//   const [state,setState] = useState({
+//     counter: 0,
+//     name: "Harshit Shukla",
+//   });
+//   console.log(state);
+//   return (
+//     <>
+//       <div onClick = { (state) => {
+//         // console.log('fr2w4e');
+//         return { ...state, counter: 10}
+//       }}>Sample</div>
+
+//     </>
+//   )
+// }
 
 const Sample = () => {
-  const {item} = useParams()
-  return <div>
-      {item} item was loaded
-  </div>;
-};
-
+  const [state, setState] = useState(1);
+  return (
+    <>
+      <div onClick = { () => {
+        setState((state) => {
+          return {...state, counter: 10};5
+        })}}>Sample
+      </div>
+    </>
+  )
+}
 export default Sample;
